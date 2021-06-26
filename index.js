ea5adc542f13d19bcdac1fcdd6b771c974336c1e
jahidhasan6484 <jahidhasan6484@gmail.com>
2021-06-26 23:15:48 +0600

    done done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.static("blog"));
 app.use(fileUpload());
 
-const serviceAccount = require("./retro-blogs-firebase-adminsdk-bipve-db862c67c9.json");
+const serviceAccount = require("./ServiceAccount/retro-blogs-firebase-adminsdk-bipve-db862c67c9.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
